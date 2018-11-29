2ecf67f99d35fb346f8e26a6d952c9eb8ded6888
Carlos Henrique <chsjr1996@gmail.com>
2018-11-29 10:31:02 -0200

    Improve pagination

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -5,14 +5,14 @@
         >
             <span class="page-link" v-if="current_page == 1">&laquo;</span>
 
-            <a class="page-link" v-else @click="changePage(1)">&laquo;</a>
+            <a class="page-link" v-else @click="changePage(1, 0)">&laquo;</a>
         </li>
 
         <li class="page-item" :class="{'active': page == current_page}" 
             :key="index"
             v-for="(page, index) in pageRange(current_page, last_page)"
         >
-            <a class="page-link" v-if="page != current_page" @click="changePage(page)">
+            <a class="page-link" v-if="page != current_page" @click="changePage(page, (index + 1))">
                 {{ page }}
             </a>
             <span v-else class="page-link">{{ page }}</span>
@@ -23,7 +23,7 @@
         >
             <span class="page-link" v-if="current_page == last_page">&raquo;</span>
 
-            <a class="page-link" v-else @click="changePage(last_page)">&raquo;</a>
+            <a class="page-link" v-else @click="changePage(last_page, 6)">&raquo;</a>
         </li>
     </ul>
 </template>
@@ -31,13 +31,26 @@
 <script>
     export default {
         props: ['current_page','last_page'],
+        data() {
+            return {
+                index: 1,
+                lastRange: []
+            }
+        },
         methods: {
             pageRange(current, max) {
 
-                // Only calculate range if have more than 3 pages
-                if (max < 4) return [1,2,3];
-
                 let arrRange   = [];
+
+                // Only calculate range if have more than 5 pages
+                if (max <= 5) {
+                    for (let i = 1; i <= max; i++) {
+                        arrRange.push(i);
+                    }
+
+                    return arrRange;
+                };
+
                 let ini        = 0;
                 let calculated = 0;
 
@@ -47,17 +60,29 @@
                 else ini = (current - 1)
 
                 // End pagination calculation
-                calculated = Math.min(ini + 2, max);
+                calculated = Math.min(ini + 4, max);
 
-                // Make range
-                for (let i = ini;i <= calculated; i++) {
-                    arrRange.push(i);
+                if (current <= 4) {
+                    arrRange = [1,2,3,4,5];
+                } else if (this.index > 4 || this.index == 1) {
+                    // Make range
+                    for (let i = ini;i <= calculated; i++) {
+                        arrRange.push(i);
+                    }
+
+                    this.lastRange = arrRange;
+
+                } else {
+                    arrRange = this.lastRange;
                 }
 
                 return arrRange;
             },
-            changePage(urlPage) {
+            changePage(urlPage, index) {
                 
+                // Get index
+                this.index = index
+
                 // Emit event to change page
                 this.$parent.$emit('changePage', urlPage)                
             }
